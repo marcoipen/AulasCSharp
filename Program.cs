89619634cf75f690e900ad4378c57748fabee705
Marco Antonio <maosilva@ipen.br>
2020-02-05 15:57:55 -0300

    Projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "esta é uma frase!";
+            string frase2 = "esta é uma frase!";
+
+            string posicao = frase.Substring(3,10);
+            Console.WriteLine(posicao);
+        }
+    }
+}
